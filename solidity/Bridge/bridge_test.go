d3d932d12e396e1e4a9762473fb88ee7e09e065d
noot <elizabethjbinks@gmail.com>
2019-02-04 11:26:56 -0500

    add setup() to bridge contract test

--- solidity/Bridge/bridge_test.go
+++ solidity/Bridge/bridge_test.go
@@ -1,14 +1,58 @@
 package bridgecontract
 
 import (
+	"crypto/ecdsa"
+	"fmt"
+	"log"
 	"io/ioutil"
+	"math/big"
 	"path/filepath"
 	"strings"
+	"testing"
+
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 )
+
+type testAccount struct {
+	addr         common.Address
+	contract     *Bridge
+	contractAddr common.Address
+	backend      *backends.SimulatedBackend
+	txOpts       *bind.TransactOpts
+}
+
+func setup() (*testAccount, error) {
+	genesis := make(core.GenesisAlloc)
+	privKey, _ := crypto.GenerateKey()
+	pubKeyECDSA, ok := privKey.Public().(*ecdsa.PublicKey)
+	if !ok {
+		return nil, fmt.Errorf("error casting public key to ECDSA")
+	}
+
+	// strip off the 0x and the first 2 characters 04 which is always the EC prefix and is not required.
+	publicKeyBytes := crypto.FromECDSAPub(pubKeyECDSA)[4:]
+	var pubKey = make([]byte, 48)
+	copy(pubKey[:], []byte(publicKeyBytes))
+
+	addr := crypto.PubkeyToAddress(privKey.PublicKey)
+	txOpts := bind.NewKeyedTransactor(privKey)
+	startingBalance, _ := new(big.Int).SetString("100000000000000000000000000000000000000", 10)
+	genesis[addr] = core.GenesisAccount{Balance: startingBalance}
+	backend := backends.NewSimulatedBackend(genesis, 210000000000)
+
+	contractAddr, _, contract, err := DeployBridge(txOpts, backend)
+	if err != nil {
+		return nil, err
+	}
+
+	return &testAccount{addr, contract, contractAddr, backend, txOpts}, nil
+}
 
 // deploys a new Ethereum contract, binding an instance of BridgeContract to it.
 func DeployBridge(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Bridge, error) {
@@ -17,7 +61,7 @@ func DeployBridge(auth *bind.TransactOpts, backend bind.ContractBackend) (common
 		return common.Address{}, nil, nil, err
 	}
 
-	fp, err := filepath.Abs("./build/Bridge.bin")
+	fp, err := filepath.Abs("../build/Bridge.bin")
 	if err != nil {
 		return common.Address{}, nil, nil, err
 	}
@@ -32,4 +76,11 @@ func DeployBridge(auth *bind.TransactOpts, backend bind.ContractBackend) (common
 		return common.Address{}, nil, nil, err
 	}
 	return address, tx, &Bridge{BridgeCaller: BridgeCaller{contract: contract}, BridgeTransactor: BridgeTransactor{contract: contract}, BridgeFilterer: BridgeFilterer{contract: contract}}, nil
+}
+
+func TestSetup(t *testing.T) {
+	_, err := setup()
+	if err != nil {
+		log.Fatalf("Can not deploy bridge contract: %v", err)
+	}
 }
