60b047f09c73cd7b07fefadd10a35096936d38a4
noot <elizabethjbinks@gmail.com>
2019-02-06 17:56:46 -0500

    add bridge contract tests; update transaction_test to use simulated backend

--- client/transaction_test.go
+++ client/transaction_test.go
@@ -1,147 +1,159 @@
 package client
 
 import (
-	"errors"
+	"context"
+	"crypto/ecdsa"
+	"fmt"
 	"io/ioutil"
-	"path/filepath"
-	"testing"
 	"math/big"
+	"path/filepath"
+	"strings"
+	"testing"
 
+	bridgecontract "github.com/ChainSafeSystems/ChainBridge/solidity/Bridge"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
-// deploys the bridge contract and setups Chain type with name, url, client, from address, password, 
-// chain ID, gas price and contract address
-func setup() (*Chain, error) {
-	chain := new(Chain)
-	chain.Name = "testnet"
-	chain.Url = "https://rinkeby.infura.io"
+type testAccount struct {
+	addr         common.Address
+	contract     *bridgecontract.Bridge
+	contractAddr common.Address
+	backend      *backends.SimulatedBackend
+	txOpts       *bind.TransactOpts
+}
 
-	client, err := ethclient.Dial(chain.Url)
+func setup() (*testAccount, error) {
+	genesis := make(core.GenesisAlloc)
+	privKey, _ := crypto.GenerateKey()
+	pubKeyECDSA, ok := privKey.Public().(*ecdsa.PublicKey)
+	if !ok {
+		return nil, fmt.Errorf("error casting public key to ECDSA")
+	}
+
+	// strip off the 0x and the first 2 characters 04 which is always the EC prefix and is not required.
+	publicKeyBytes := crypto.FromECDSAPub(pubKeyECDSA)[4:]
+	var pubKey = make([]byte, 48)
+	copy(pubKey[:], []byte(publicKeyBytes))
+
+	addr := crypto.PubkeyToAddress(privKey.PublicKey)
+	txOpts := bind.NewKeyedTransactor(privKey)
+	txOpts.GasLimit = 6700000
+	startingBalance, _ := new(big.Int).SetString("100000000000000000000000000000000000000", 10)
+	genesis[addr] = core.GenesisAccount{Balance: startingBalance}
+	backend := backends.NewSimulatedBackend(genesis, 210000000000)
+
+	contractAddr, _, contract, err := DeployBridge(txOpts, backend)
 	if err != nil {
 		return nil, err
 	}
 
-	chain.Client = client
+	return &testAccount{addr, contract, contractAddr, backend, txOpts}, nil
+}
 
-	fp, err := filepath.Abs("../keystore")
+func logs(test *testAccount) ([]types.Log, error) {
+	query := ethereum.FilterQuery{
+		Addresses: []common.Address{
+			test.contractAddr,
+		},
+		FromBlock: big.NewInt(0),
+	}
+
+	logs, err := test.backend.FilterLogs(context.Background(), query)
 	if err != nil {
 		return nil, err
 	}
 
-	ks := keystore.NewKeyStore(fp, keystore.StandardScryptN, keystore.StandardScryptP)
-	ksaccounts := ks.Accounts()
-	if len(ksaccounts) == 0 {
-		return nil, errors.New("no accounts in keystore")
+	fmt.Println(logs)
+	return logs, err
+}
+
+// deploys a new Ethereum contract, binding an instance of BridgeContract to it.
+func DeployBridge(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *bridgecontract.Bridge, error) {
+	bridgeabi, err := abi.JSON(strings.NewReader(bridgecontract.BridgeABI))
+	if err != nil {
+		return common.Address{}, nil, nil, err
 	}
 
-	a := common.HexToAddress("0x8f9b540b19520f8259115a90e4b4ffaeac642a30")
-	chain.From = &a
-
-	fp, err = filepath.Abs("../solidity/build/Bridge.bin")
+	fp, err := filepath.Abs("../build/Bridge.bin")
 	if err != nil {
-		return nil, err
+		return common.Address{}, nil, nil, err
 	}
 
 	bin, err := ioutil.ReadFile(fp)
 	if err != nil {
-		return nil, err
+		return common.Address{}, nil, nil, err
 	}
 
-	chain.Password = "password"
-	chain.Id = big.NewInt(0)
-	chain.GasPrice = big.NewInt(10000000)
-
-	address, err := Deploy(*chain, ks, bin, "Bridge")
+	address, tx, contract, err := bind.DeployContract(auth, bridgeabi, bin, backend)
 	if err != nil {
-		return nil, err
+		return common.Address{}, nil, nil, err
 	}
-
-	chain.Contract = &address
-
-	return chain, nil
+	return address, tx, &bridgecontract.Bridge{BridgeCaller: bridgecontract.BridgeCaller{contract: contract}, BridgeTransactor: bridgecontract.BridgeTransactor{contract: contract}, BridgeFilterer: bridgecontract.BridgeFilterer{contract: contract}}, nil
 }
 
-func TestSendTx(t *testing.T) {
-	chain, err := setup()
+func TestSetup(t *testing.T) {
+	_, err := setup()
 	if err != nil {
-		t.Fatal(err)
-	}
-
-	_, err = SendTx(chain, big.NewInt(0), []byte{})	
-	if err != nil {
-		t.Fatal(err)
+		t.Errorf("Can not deploy bridge contract: %v", err)
 	}
 }
 
-// func TestWithdraw(t *testing.T) {
+// deploys the bridge contract and setups Chain type with name, url, client, from address, password, 
+// chain ID, gas price and contract address
+// func setup() (*Chain, error) {
 // 	chain := new(Chain)
 // 	chain.Name = "testnet"
+// 	chain.Url = "https://rinkeby.infura.io"
 
-// 	client, err := core.NewConnection()
+// 	client, err := ethclient.Dial(chain.Url)
 // 	if err != nil {
-// 		t.Error(err)
+// 		return nil, err
 // 	}
+
 // 	chain.Client = client
 
-// 	contract, err := core.ContractAddress("Bridge", "testnet")
+// 	fp, err := filepath.Abs("../keystore")
 // 	if err != nil {
-// 		t.Error(err)
+// 		return nil, err
 // 	}
-// 	*chain.Contract = contract
 
-// 	w := new(Withdrawal)
-// 	w.Recipient = "0xc84233646c0aa920c66a2e220ea790e548b72f9e"
-// 	w.Value = big.NewInt(7777)
-// 	w.FromChain = "4"
-// 	err = Withdraw(chain, w)
+// 	ks := keystore.NewKeyStore(fp, keystore.StandardScryptN, keystore.StandardScryptP)
+// 	ksaccounts := ks.Accounts()
+// 	if len(ksaccounts) == 0 {
+// 		return nil, errors.New("no accounts in keystore")
+// 	}
+
+// 	a := common.HexToAddress("0x8f9b540b19520f8259115a90e4b4ffaeac642a30")
+// 	chain.From = &a
+
+// 	fp, err = filepath.Abs("../solidity/build/Bridge.bin")
 // 	if err != nil {
-// 		t.Error(err)
+// 		return nil, err
 // 	}
-// }
 
-// func test_AddAuthority(t *testing.T) {
-// 	chain := new(Chain)
-// 	chain.Name = "testnet"
-
-// 	client, err := core.NewConnection("http://localhost:8545")
+// 	bin, err := ioutil.ReadFile(fp)
 // 	if err != nil {
-// 		t.Error(err)
+// 		return nil, err
 // 	}
-// 	chain.Client = client
 
-// 	contract, err := core.ContractAddress("Bridge", "testnet")
+// 	chain.Password = "password"
+// 	chain.Id = big.NewInt(0)
+// 	chain.GasPrice = big.NewInt(10000000)
+
+// 	address, err := Deploy(*chain, ks, bin, "Bridge")
 // 	if err != nil {
-// 		t.Error(err)
+// 		return nil, err
 // 	}
-// 	*chain.Contract = contract
 
-// 	err = AddAuthority(chain, "0xc84233646c0aa920c66a2e220ea790e548b72f9e")
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// }
+// 	chain.Contract = &address
 
-// func test_Deposit(t *testing.T) {
-// 	chain := new(Chain)
-// 	chain.Name = "testnet"
-
-// 	client, err := core.NewConnection("http://localhost:8545")
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// 	chain.Client = client
-
-// 	contract, err := core.ContractAddress("Bridge", "testnet")
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// 	*chain.Contract = contract
-
-// 	err = Deposit(chain, big.NewInt(77), "5")
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+// 	return chain, nil
 // }
--- solidity/Bridge/bridge_test.go
+++ solidity/Bridge/bridge_test.go
@@ -1,7 +1,9 @@
 package bridgecontract
 
 import (
+	"context"
 	"crypto/ecdsa"
+	"encoding/hex"
 	"fmt"
 	"io/ioutil"
 	"math/big"
@@ -9,6 +11,7 @@ import (
 	"strings"
 	"testing"
 
+	ethereum "github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
@@ -54,6 +57,23 @@ func setup() (*testAccount, error) {
 	return &testAccount{addr, contract, contractAddr, backend, txOpts}, nil
 }
 
+func logs(test *testAccount) ([]types.Log, error) {
+	query := ethereum.FilterQuery{
+		Addresses: []common.Address{
+			test.contractAddr,
+		},
+		FromBlock: big.NewInt(0),
+	}
+
+	logs, err := test.backend.FilterLogs(context.Background(), query)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(logs)
+	return logs, err
+}
+
 // deploys a new Ethereum contract, binding an instance of BridgeContract to it.
 func DeployBridge(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Bridge, error) {
 	bridgeabi, err := abi.JSON(strings.NewReader(BridgeABI))
@@ -96,6 +116,11 @@ func TestDeposit(t *testing.T) {
 	if err != nil {
 		t.Error("could not deposit into bridge")
 	}
+
+	_, err = logs(test)
+	if err != nil {
+		t.Fatalf("Unable to get logs of bridge contract: %v", err)
+	}
 }
 
 func TestFundBridge(t *testing.T) {
@@ -108,5 +133,60 @@ func TestFundBridge(t *testing.T) {
 	_, err = test.contract.FundBridge(test.txOpts)
 	if err != nil {
 		t.Error("could not fund bridge")
+	}
+}
+
+// should try to withdraw more than deposited
+func TestWithdrawToFailing(t *testing.T) {
+	test, err := setup()
+	if err != nil {
+		t.Errorf("Can not deploy bridge contract: %v", err)
+	}	
+
+	test.txOpts.Value = big.NewInt(1000000000000000000)
+	_, err = test.contract.WithdrawTo(test.txOpts, test.addr, big.NewInt(1), big.NewInt(2000000000000000000))
+	if err == nil {
+		t.Error("could withdraw more than balance")
+	}
+}
+
+// try to call withdraw from an address that is not the authority
+func TestWithdrawWrongAddress(t *testing.T) {
+	test, err := setup()
+	if err != nil {
+		t.Errorf("Can not deploy bridge contract: %v", err)
+	}	
+
+	test.txOpts.Value = big.NewInt(1000000000000000000)
+	txHashBytes, err := hex.DecodeString("09ed879028eb8a0b28763584639d0d609a42d4263b90ed3635323502aa9efde5")
+	if err != nil {
+		t.Error(err)
+	}
+
+	txHash := [32]byte{}
+	copy(txHash[:], txHashBytes)
+	_, err = test.contract.Withdraw(test.txOpts, common.HexToAddress("0x01"), big.NewInt(10), big.NewInt(1), txHash)
+	if err == nil {
+		t.Error("could withdraw from non authority account")
+	}
+}
+
+func TestWithdraw(t *testing.T) {
+	test, err := setup()
+	if err != nil {
+		t.Errorf("Can not deploy bridge contract: %v", err)
+	}	
+
+	test.txOpts.Value = big.NewInt(1000000000000000000)
+	txHashBytes, err := hex.DecodeString("09ed879028eb8a0b28763584639d0d609a42d4263b90ed3635323502aa9efde5")
+	if err != nil {
+		t.Error(err)
+	}
+
+	txHash := [32]byte{}
+	copy(txHash[:], txHashBytes)
+	_, err = test.contract.Withdraw(test.txOpts, test.addr, big.NewInt(10), big.NewInt(1), txHash)
+	if err != nil {
+		t.Error("could not withdraw")
 	}
 }
